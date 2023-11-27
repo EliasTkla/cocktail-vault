18c6c1e14fe2cfc9312253f091761d840ec6654e
Elias Tokola <eliastokola88@gmail.com>
2023-11-26 21:04:28 -0500

    fixed type error on home links

--- app/components/buttons/LearnButton.tsx
+++ app/components/buttons/LearnButton.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import styles from './styles/learnButton.module.css';
+import Link from 'next/link';
 
 export default function CustomButton({ page, text }: { page: string, text: string }) {
-    const router = useRouter();
 
     return (
-        <button className={styles.learn_button} title='about page link' onClick={() => { router.push(`/pages/${page}`) }}>{text}</button>
+        <Link className={styles.learn_button} href={page === "about" ? "/pages/about" : "/pages/collection"} as={`/pages/${page}`}>
+            {text}
+        </Link>
     )
 }
